e3103f46c6ef7c100ccda4e1ef3858ef9cde64eb
Will Bennion <will.bennion@outlook.com>
2017-11-19 13:37:35 +0000

    Added more comments to client-side JS and fixed formatting in messages.md.

--- client/js/app.js
+++ client/js/app.js
@@ -21,27 +21,30 @@ var store = new Vuex.Store({
 	},
 
 	mutations: {
+		/** Updates, creates or deletes a game lobby based on a data message received from the server. */
 		updateLobby: function(state, msg) {
 			// Find existing lobby with same ID
-			var i;
-			for (i = 0; i < state.lobbies.length; i++)
+			for (var i = 0; i < state.lobbies.length; i++)
 				if (state.lobbies[i].gameId == msg.gameId)
 					break;
 			
+			// If there was an item in the list with the same ID as the one received from the sever
 			if (i != state.lobbies.length) {
-				if (msg.gameClosed) {
-					// Remove from array
+				if (msg.gameClosed)
+					// If the server has told us the lobby has closed, remove from array
 					state.lobbies.splice(i, 1);
 
-				} else {
-					// Update existing
-					var lobby = state.lobbies[i];
-					lobby.player1 = msg.player1;
-					lobby.player2 = msg.player2;
-					lobby.gameState = msg.gameState;
+				else {
+					// Otherwise, update the existing lobby
+					var lob = state.lobbies[i];
+					lob.player1 = msg.player1;
+					lob.player2 = msg.player2;
+					lob.gameState = msg.gameState;
 				}
+			
+			// If an item with matching ID was not found and this new lobby is not closing
 			} else if (!msg.gameClosed) {
-				// If one was not found, add it
+				// Add the new lobby to the list
 				state.lobbies.push({
 					gameId: msg.gameId,
 					player1: msg.player1,
@@ -51,13 +54,11 @@ var store = new Vuex.Store({
 			}
 		},
 
+		/** Updates the game state based on data received from the server.
+		 * Will update marble count and allow or block the user from playing a turn. */
 		updateGameState: function(state, data) {
 			state.marbles = data.marbles;
 			state.yourTurn = state.canPlay = data.yourTurn;
-		},
-
-		setProperty: function(state, prop, val) {
-			state[prop] = val;
 		}
 	}
 });
@@ -67,86 +68,110 @@ var store = new Vuex.Store({
 // ----------------//
 var ws = new WebSocket("ws://" + location.host);
 
+// If the socket closes, we have lost connection to the server so show an error message
 ws.addEventListener('close', function(e) {
 	$('#network-error-modal').modal('show');
 });
 
+// When we receive data from the server, attempt to convert it to an oject and then handle it
+// with the relevant function from the dictionary below.
 ws.addEventListener('message', e => {
 	var data = JSON.parse(e.data);
 	if (!data.event) return;
 	var f = MessageHandlers[data.event];
-	f && f(data);
+	f && f(data, store.state);
 });
 
 /** Dictionary containing handlers for all WebSocket messages.
- * @type {Object.<string,(any)=>void>} */
+ * @type {Object.<string,(message:any,state:any)=>void>} */
 var MessageHandlers = {
-	// Core functions
+// Core functions
 	"heartbeat": function() {
 		wsSend({event: "beat"});
 	},
 
-	// Lobby functions
+// Lobby functions
 	"lobby-join": function() {
+		// Hide loading message and go to the lobby screen
 		router.replace("/lobby");
 		applicationLoading(false);
 	},
-	"chat-message": function(data) {
-		store.state.messages.push(data.message);
+	"chat-message": function(msg, state) {
+		// Add the chat message to the global store
+		state.messages.push(msg.message);
 	},
-	"game-create": function(data) {
+	"game-create": function(msg, state) {
+		// Hide loading message and if successfully created lobby then show the waiting box
 		applicationLoading(false);
-		if (data.success)
-			store.state.inGameLobby = data.gameId;
+		if (msg.success)
+			state.inGameLobby = msg.gameId;
 	},
-	"game-join": function(data) {
+	"game-join": function() {
+		// Hide loading message
 		applicationLoading(false);
 	},
-	"game-status-update": function(data) {
-		store.commit('updateLobby', data);
+	"game-status-update": function(msg) {
+		// Pass the lobby data to the store's `updateLobby` mutator
+		store.commit('updateLobby', msg);
 	},
 
-	// Game functions
-	"game-start": function(data) {
+// Game functions
+	"game-start": function(msg, state) {
+		// Hide loading and go to game screen
 		applicationLoading(false);
 		router.replace("/game");
-		store.commit('updateGameState', data);
-		store.state.gameState = "in-game";
-		// Now we've joined, reset game lobby flag so that however the game ends we can create a new game
-		store.state.inGameLobby = -1;
+
+		// Update the application state
+		store.commit('updateGameState', msg);
+		state.gameState = "in-game";
+		state.inGameLobby = -1; // (no longer waiting in lobby so turn this off before the user returns to lobby)
 	},
-	"game-update": function(data) {
-		store.commit('updateGameState', data);
+	"game-update": function(msg) {
+		// Pass the game data to the store's `updateGameState` mutator
+		store.commit('updateGameState', msg);
 	},
-	"play-turn": function(data) {
-		if (!data.success) {
-			store.state.canPlay = true;
-			store.state.lastErrorMessage = data.reason;
+	"play-turn": function(msg, state) {
+		// If the move was invalid
+		if (!msg.success) {
+			// Allow the player to try again and show them the error
+			state.canPlay = true;
+			state.lastErrorMessage = msg.reason;
 		}
 	},
-	"game-over": function(data) {
-		store.state.yourTurn = store.state.canPlay = false;
-		store.state.gameState = "game-over";
-		store.state.lastWinner = data.win;
-		store.state.rematchStatus = data.ai ? "The AI is always ready for a rematch." : "Your opponent has not voted on whether they want to rematch.";
-		store.state.allowRematchVote = true;
+	"game-over": function(msg, state) {
+		// Game over so the user cannot play their turn
+		state.yourTurn = state.canPlay = false;
+		// Update game state
+		state.gameState = "game-over";
+		state.lastWinner = msg.win;
+		// Show a rematch message based on whether the user was playing against AI or not and allow them to vote for a rematch.
+		state.rematchStatus = msg.ai ? "The AI is always ready for a rematch." : "Your opponent has not voted on whether they want to rematch.";
+		state.allowRematchVote = true;
 	},
-	"game-leave": function(data) {
-		if (data.success) {
-			store.state.inGameLobby = -1;
+	"game-leave": function(msg, state) {
+		// If the server allowed you to leave the game
+		if (msg.success) {
+			// User no longer waiting in a lobby and ensure user is on lobby screen
+			state.inGameLobby = -1;
 			router.replace("/lobby");
 		}
 	},
-	"game-terminate": function(_) {
-		store.state.yourTurn = store.state.canPlay = false;
-		store.state.gameState = "opponent-forfeit";
+	"game-terminate": function(_, state) {
+		// Game now over so user cannot play a turn
+		state.yourTurn = state.canPlay = false;
+		// Update game state (which will cause forefeit notification to show)
+		state.gameState = "opponent-forfeit";
 	},
-	"rematch-vote": function(data) {
-		store.state.rematchStatus = "Your opponent has " + (data.opponentVote ? "voted to rematch." : "chosen not to rematch.");
-		store.state.allowRematchVote = data.opponentVote;
+	"rematch-vote": function(msg, state) {
+		// Update the rematch text to show what user's opponent voted and disable button if opponent returned to lobby.
+		state.rematchStatus = "Your opponent has " + (msg.opponentVote ? "voted to rematch." : "chosen not to rematch.");
+		state.allowRematchVote = msg.opponentVote;
 	}
 };
 
+/** Sends a data message to the server.
+ * @param {*} data The object to send. Should have an `event` property.
+ */
 function wsSend(data) {
 	ws.send(JSON.stringify(data));
 }
@@ -160,12 +185,15 @@ var chatPanel = Vue.component('chat-panel', {
 		messageInput: ""
 	};},
 	methods: {
+		/** Handler that fires when the message form gets submitted.
+		 * Sends user's chat message to server and clears text input. */
 		submitMessage: function(e) {
 			wsSend({event: "chat-message", message: this.messageInput});
 			this.messageInput = "";
 		}
 	},
 	filters: {
+		/** Formats a number into a time in the format HH:MM:SS. */
 		date: function (v) {
 			var d = new Date(v);
 			function td(v) { return (v < 10 ? "0" : "") + v; } // two digits
@@ -178,6 +206,8 @@ var bsModal = Vue.component('bs-modal', {
 	props: ["title", "buttons", "closeButton"],
 	template: '#template-bs-modal',
 	methods: {
+		/** Raises an event when a button is clicked.
+		 * Event name is set in the `buttons` property for this component. */
 		buttonCallback: function(eventName) {
 			if (eventName)
 				this.$emit(eventName);
@@ -192,6 +222,8 @@ var ViewWelcome = {
 	template: '#view-welcome',
 
 	methods: {
+		/** Handler for welcome screen name submission form.
+		 * Sends username to server and requests to join main lobby. */
 		enterName: function() {
 			applicationLoading(true);
 			wsSend({event: "lobby-join", username: store.state.username});
@@ -206,9 +238,13 @@ var ViewLobby = {
 	};},
 
 	methods: {
+		/** Handler for the button to create a new lobby. */
 		submitCreateGameLobby: function() {
+			// Show loading modal and hide the create game popover
 			applicationLoading(true);
 			this.$data.showCreateGamePopover = false;
+
+			// Send a `game-create` request to the server with the settings the user chose.
 			wsSend({
 				event: "game-create",
 				difficulty: jQuery('[name="difficultyOptions"]:checked').val(),
@@ -216,8 +252,10 @@ var ViewLobby = {
 			});
 		},
 
+		/** Handler for a "Join" button on the lobby list. */
 		joinGameLobby: function(gameId) {
 			if (gameId == store.state.inGameLobby) return; // Do nothing if we're already in the clicked lobby
+			// Show loading screen and send a `game-join` request to the server with the ID of the game to join
 			applicationLoading(true);
 			wsSend({
 				event: "game-join",
@@ -225,6 +263,8 @@ var ViewLobby = {
 			});
 		},
 
+		/** Handler for button to indicate to the server the user wishes to stop waiting for a second player
+		 * in the lobby they created. */
 		cancelLobbyWaiting: function() {
 			wsSend({ event: "game-leave" });
 		}
@@ -239,14 +279,20 @@ var ViewGame = {
 	};},
 
 	methods: {
+		/** Handler for the "Play turn" button. */
 		playTurn: function() {
 			wsSend({ event: "play-turn", marbles: this.$data.marblesToRemove });
 			store.state.canPlay = false; // Prevent the user from making a second move before the server has responded
 		},
+
+		/** Handler for the "Forefeit game" button and the "Return to lobby" buttons. */
 		gameLeave: function() {
 			wsSend({ event: "game-leave" });
 		},
+
+		/** Handler for the "Vote rematch" button on game end. */
 		voteRematch: function() {
+			// Update the text to give feedback the user that they have clicked it.
 			store.state.rematchStatus = "You have voted to rematch. Waiting on your opponent.";
 			wsSend({ event: "rematch-vote", vote: true });
 		}
@@ -278,17 +324,20 @@ var app = new Vue({
 
 	// Application methods
 	methods: {
+		/** Handler for the "Lost connection" modal's "Reload" button. */
 		reload: function() { window.location.reload(); },
 	}
 
 }).$mount('#app');
 
-// Setup tooltips
+// Setup tooltips on page load
 $(function () {
-	$('[data-toggle="tooltip"]').tooltip()
-})
+	$('[data-toggle="tooltip"]').tooltip();
+});
 
-// Use to turn on/off loading dialog
+/** Function to turn on or off loading modal dialog over the application.
+ * @param {boolean} v Set to `true` to show loading, `false` to hide.
+*/
 function applicationLoading(v) {
 	jQuery('#app-loading-modal').modal(v ? "show" : "hide");
 }
